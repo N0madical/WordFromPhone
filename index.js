a79af50ddee4e9b9a6a292258c20c7d68ba4de9f
Nomadical <nomadicalyt@gmail.com>
2023-09-22 20:06:59 -0500

    finished

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
+run = false
+
 function findWords() {
-    wordlist = []
-    letters = {
-        1: 1,
-        2: ["a","b","c"],
-        3: ["d","e","f"],
-        4: ["g","h","i"],
-        5: ["j","k","l"],
-        6: ["m","n","o"],
-        7: ["p","q","r"],
-        8: ["s","t","u"],
-        9: ["v","x","y","z"],
-        0: 1,
+    if(!run) {
+        phonenumber = document.getElementById('box1').value + document.getElementById('box2').value + document.getElementById('box3').value
+        if(phonenumber.length == 10){
+            document.getElementById('warning').style.color = "black"
+            document.getElementById('startbutton').innerHTML = "Running..."
+            wordlist = [];
+            for(i = 0; i < 11; i++) {
+                for(j = (i+1); j < 11; j++) {
+                    if(((j-i) >= document.getElementById('minlen').value) && ((j-i) <= document.getElementById('maxlen').value))
+                    wordlist.push(phonenumber.slice(i,j))
+                }
+            }
+            q = 0
+            offset = 0
+            document.getElementById('outputbox').innerHTML = ""
+            run = true
+        } else {
+            document.getElementById('warning').style.color = "red"
+        }   
+    } else {
+        run = false
+        document.getElementById('startbutton').innerHTML = "Find Words"
     }
-    lengths = {
-        1:1,
-        2:3,
-        3:3,
-        4:3,
-        5:3,
-        6:3,
-        7:3,
-        8:3,
-        9:3,
-        0:1,
-    }
-
-    phonenumber = `${document.getElementById("box1").value}${document.getElementById("box2").value}${document.getElementById("box3").value}`
-    document.getElementById("texthere").value += phonenumber
-
-    
 }
+
+function findEngine() {
+    if(run) {
+        console.debug(wordlist[q])
+        if(words[wordlist[q]] != undefined) {
+            wordsarray = words[wordlist[q]]
+            containernumber = phonenumber.split(wordlist[q])
+            console.debug(containernumber[0], wordsarray, containernumber[1])
+            for(i = 0; i < wordsarray.length; i++) {
+                fill = containernumber[0]+wordsarray[i]+containernumber[1]
+                fill = "(" + fill.slice(0,3) + ")-" + fill.slice(3,6) + "-" + fill.slice(6,11)
+                document.getElementById('outputbox').innerHTML += `<p style="top: ${Math.floor(offset/4)*10}%; left: ${(offset%4)*25}%; font-size:20px;">${wordsarray[i]}</p>`
+                document.getElementById('outputbox').innerHTML += `<p style="top: ${(Math.floor(offset/4)*10)+4}%; left: ${(offset%4)*25}%";>${fill}</p>`
+                offset += 1
+            }
+        }
+        if(q >= wordlist.length) {
+            run = false
+            document.getElementById('startbutton').innerHTML = "Find Words"
+        } else {
+            q++
+        }
+    }
+}
+
+function validate(field, type, max="", min="") {
+    if(type == 0) {
+        regex = RegExp('[^A-Z,a-z]')
+        field.value = field.value.replace(regex, '')
+        if(max != "") {
+            field.value = field.value.slice(0,max)
+        }
+    } else if (type == 1) {
+        regex2 = RegExp('[^0-9]')
+        field.value = field.value.replace(regex2, '')
+        if(max != "") {
+            if (field.value > max) {field.value = max}
+        }
+        if(min != "") {
+            if ((field.value < min) && (field.value != "")) {field.value = min}
+        }
+    }
+}
+
+setInterval(findEngine, 1);
